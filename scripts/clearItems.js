2e8bead7c1dbf0ad1ed4079106f9d9f84b164a76
Soufiane ISAM <isam.soufiane1@gmail.com>
2025-01-12 16:07:30 +0100

    Latest

--- scripts/clearItems.js
+++ scripts/clearItems.js
@@ -1,14 +1,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const contractAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const Marketplace = await hre.ethers.getContractAt(
     "Marketplace",
     contractAddress
   );
 
-  const tx = await Marketplace.clearItems();
-  await tx.wait();
+  // const tx = await Marketplace.clearItems();
+  // await tx.wait();
 
   const count = await Marketplace.itemCount();
   console.log("All items cleared! Current Count: " + count.toString());
